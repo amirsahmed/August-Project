ddddfb209f6646c274af8612bc149c170141a935
amirsahmed <amirsahmed@outlook.com>
2020-08-24 19:06:10 -0400

    commit version 3

--- src/test/java/com/MatchMaker.java
+++ src/test/java/com/MatchMaker.java
@@ -4,10 +4,11 @@ public class MatchMaker {
 
     public static void main(String[] args) {
 
-        String name1 = "Amir";
+        String name1 = "";
         String name2 = "Nigi";
         String name3 = "Mili";
 
+        System.out.println("Mili made a change!");
 
 
 
